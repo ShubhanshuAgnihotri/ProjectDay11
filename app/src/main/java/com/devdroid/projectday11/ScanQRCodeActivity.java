63a82da3c2629311e7609dedbc2058b63bc9c156
shubh <shubhanshuagnihotri16@gmail.com>
2024-03-15 22:51:06 +0530

    ProjectDay11

---
+++ app/src/main/java/com/devdroid/projectday11/ScanQRCodeActivity.java
@@ -0,0 +1,53 @@
+package com.devdroid.projectday11;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class ScanQRCodeActivity extends AppCompatActivity {
+
+    Button btnScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qrcode);
+
+        btnScan = (Button) findViewById(R.id.btnScan);
+
+        btnScan.setOnClickListener(v->
+        {
+            ScanCode();
+        });
+    }
+
+    private void ScanCode() {
+        ScanOptions  options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureActivity.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result->{
+        if(result.getContents() != null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(ScanQRCodeActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+}
